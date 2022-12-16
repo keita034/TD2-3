1784b3fd03743181cae217af1650eef030ab341e
masato izumida <Oioi_0126@outlook.com>
2022-12-17 01:01:12 +0900

    マウスのカメラ操作

--- UserCamera.h
+++ UserCamera.h
@@ -1,21 +1,43 @@
 #pragma once
+
+#include "Matrix4.h"
 #include"Vector2.h"
 #include"Input.h"
 
 class UserCamera {
 
-private:
-
-	UserCamera();
+public:
+	
+	UserCamera(int window_width, int window_height);
 
 	void Initialize();
 
-
 	void Update();
 
-public:
+	void MultiplyMatrix(const AliceMathF::Matrix4& matrix);
 
-	AliceMathF::Vector2 mouseMovement = { 0.0f,0.0f };
+	AliceMathF::Vector3 Vector3Transform(const AliceMathF::Vector3& v, const AliceMathF::Matrix4& m);
+
+	AliceMathF::Vector3 GetEye() { return vTargetEye; }
+	AliceMathF::Vector3 GetUp() { return vUp; }
+
+private:
+	// カメラ注視点までの距離
+	float distance_ = 50;
+	AliceMathF::Vector2 mousePos = { 0.0f,0.0f };
+	AliceMathF::Vector2 oldMousePos = { 0.0f,0.0f };
 	Input* input_ = nullptr;
+
+	// 回転行列
+	AliceMathF::Matrix4 matRot;
+
+	// スケーリング
+	float scaleX_ = 1.0f;
+	float scaleY_ = 1.0f;
+
+	AliceMathF::Vector3 vTargetEye;
+	AliceMathF::Vector3 vUp;
+
+
 
 };
